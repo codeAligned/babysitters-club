a3dd5174cd796e664ade6d2fbaf1363014b76807
chriswigington <chriswigington@gmail.com>
2016-08-12 16:25:56 -0400

    Adds method to accept BookingRequests and create Bookings

--- app/models/booking_request.rb
+++ app/models/booking_request.rb
@@ -1,4 +1,9 @@
 class BookingRequest < ApplicationRecord
   belongs_to :parent
   belongs_to :babysitter
+
+  def accept()
+    Booking.create({parent_id: self.parent_id, babysitter_id: self.babysitter_id, datetime: self.datetime, duration: self.duration})
+    # destroy booking request
+  end
 end
