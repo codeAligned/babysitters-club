6cf5507984946ce390cd0a3af7520d4f11ab1f76
grahk93 <grace.juster@flatironschool.com>
2016-08-12 16:23:47 -0400

    added search still need to know if works

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,12 +1,18 @@
 class ParentsController < ApplicationController 
 
 	def index 
-		render json: Parent.all, includes:['babysitters','requests','bookings'] 
+		if params[:search]
+			parents = Parent.search(params[:search])
+		else
+			parents = Parent.all
+		end
+		render json: parents, includes:['babysitters','requests','bookings'] 
 	end 
 
 	def show
 		parent = Parent.find(params[:id])
 		render json: parent
-	end 
+	end  
 
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812173044) do
+ActiveRecord::Schema.define(version: 20160812200359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20160812173044) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "address"
+    t.integer  "kid_count"
   end
 
   create_table "requests", force: :cascade do |t|
