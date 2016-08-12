96a92433990479219e21bfb3ccab1d9210fa0085
grahk93 <grace.juster@flatironschool.com>
2016-08-12 14:59:58 -0400

    changed to datetime object in bookings can sort by datetime

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,13 @@
+require "byebug"
+
 class Booking < ApplicationRecord
   belongs_to :parent
   belongs_to :babysitter
 
   #sort bookings by date 
-  def self.sortByDate
-  	self.sort_by do |booking| 
-  		booking.date
+  def self.sort_by_date_time
+  	self.all.sort_by do |booking| 
+  		booking.datetime
   	end
   end
 end
--- db/migrate/20160812151446_create_bookings.rb
+++ db/migrate/20160812151446_create_bookings.rb
@@ -3,8 +3,7 @@ class CreateBookings < ActiveRecord::Migration[5.0]
     create_table :bookings do |t|
       t.references :parent, foreign_key: true
       t.references :babysitter, foreign_key: true
-      t.date :date
-      t.time :time
+      t.datetime :datetime
       t.integer :duration
 
       t.timestamps
