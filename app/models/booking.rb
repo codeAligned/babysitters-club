28a32b28f63f859fd4e158f20438ead06cb8ead4
grahk93 <grace.juster@flatironschool.com>
2016-08-12 13:28:26 -0400

    can sort bookings by date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :parent
   belongs_to :babysitter
+
+  #sort bookings by date 
+  def self.sortByDate
+  	self.sort_by do |booking| 
+  		booking.date
+  	end
+  end
 end
