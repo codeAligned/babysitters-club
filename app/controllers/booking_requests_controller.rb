6691f6fa9dfa9207879f930041f3e91915cc6930
chriswigington <chriswigington@gmail.com>
2016-08-12 16:45:35 -0400

    Adds controller methods for BookingRequests and Reviews

--- app/controllers/booking_requests_controller.rb
+++ app/controllers/booking_requests_controller.rb
@@ -1,2 +1,11 @@
 class BookingRequestsController < ApplicationController
+  def index
+    requests = BookingRequest.all
+    render json: requests
+  end
+
+  def show
+    request = BookingRequest.find(params[:id])
+    render json: request
+  end
 end
