65f62dae5abfeb13c38f2310b0450f2aba1fb81a
chriswigington <chriswigington@gmail.com>
2016-08-17 18:24:03 -0400

    Small formatting changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authenticate, only: [:create]
-  
+
   def index
 		users = User.all
 		render json: users
@@ -8,7 +8,6 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
-    byebug
     # parent = Parent.create(parents_params)
     if user_type_params[:user_type]=='PARENT'
       account = Parent.create({user_id: user.id})
