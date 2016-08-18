a31e956a1c0456c1be572b0c627413dde2d6e2fb
gyeon <gyeon19@gmail.com>
2016-08-18 09:43:37 -0400

    booking serializer, booking controller

--- app/controllers/api/v1/booking_requests_controller.rb
+++ app/controllers/api/v1/booking_requests_controller.rb
@@ -10,8 +10,12 @@ class Api::V1::BookingRequestsController < ApplicationController
   end
 
   def create
-    request = BookingRequest.create({parent_id: params[:parent_id].to_i, babysitter_id: params[:babysitter_id].to_i, duration: params[:duration].to_i})
-    		# parent = Parent.create(parents_params) #dont forget desired time
+    byebug
+
+    request = BookingRequest.create(id: params[:id])
+#{parent_id: params[:parent_id].to_i, babysitter_id: params[:babysitter_id].to_i, duration: params[:duration].to_i}
+
+        # parent = Parent.create(parents_params) #dont forget desired time
     render json: request
   end
 
@@ -23,7 +27,7 @@ class Api::V1::BookingRequestsController < ApplicationController
 private
 
 	 def booking_requests_params
-		 params.require(:booking_requests).permit(:parent_id, :babysitter_id, :duration, :desired_time)
+		 params.require(:user).permit(:id, :babysitter_id, :duration, :desired_time)
 	 end
 
 
