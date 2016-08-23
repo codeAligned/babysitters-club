3fe9ec8804b90b790900943341d7722f60fdd66f
chriswigington <chriswigington@gmail.com>
2016-08-23 18:51:32 -0400

    Correct error in migration

--- db/migrate/20160822231223_add_parent_id_babysitter_id_to_review.rb
+++ db/migrate/20160822231223_add_parent_id_babysitter_id_to_review.rb
@@ -1,6 +1,5 @@
 class AddParentIdBabysitterIdToReview < ActiveRecord::Migration[5.0]
   def change
-    add_column :reviews, :review, :string
     add_reference :reviews, :babysitter, foreign_key: true
     add_reference :reviews, :parent, foreign_key: true
   end
